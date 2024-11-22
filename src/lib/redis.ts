5d44cfef8c5489e2899172344d34d410e80d5a65
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2024-11-22 04:56:02 +0200

    update redis config

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -5,6 +5,7 @@ import { env } from '../env';
 export const createRedis = () =>
   new Redis(env.REDIS_URL, {
     maxRetriesPerRequest: null,
+    connectTimeout: 10000 * 10,
     tls: {
       rejectUnauthorized: false,
     },
